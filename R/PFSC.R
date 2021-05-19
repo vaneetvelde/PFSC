4fdebcd150999a3d3f1dc9992953b30049c1707b
Hans Van Eetvelde <Hans.VanEetvelde@UGent.be>
2021-05-19 16:56:52 +0200

    add authors

--- R/PFSC.R
+++ R/PFSC.R
@@ -1,8 +1,12 @@
 #' Probabilistic final standing calculator
 #'
 #' Functions to create the probabilistic final standings of prematurely stopped leagues.
+#' The main functions are \code{\link{league_table}}, \code{\link{team_ratings}} and \code{\link{simulate_league}}
 #'
 #' @docType package
 #' @author Hans Van Eetvelde <Hans.VanEetvelde@@UGent.be>
+#' @author Christophe Ley
+#' @author Lars Magnus Hvattum
 #' @name PFSC
+#' @references Van Eetvelde, H., Hvattum, L. M., & Ley, C. (2021). The Probabilistic Final Standing Calculator: a fair stochastic tool to handle abruptly stopped football seasons. arXiv preprint arXiv:2101.10597.
 NULL
